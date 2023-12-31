57895fc17c007c2c710ad4b9f130e257ee52a523
Daniel Markow <d_markow@icloud.com>
2023-12-31 15:08:32 +0100

    redirecting to home if there is no session

--- src/app/config/layout.tsx
+++ src/app/config/layout.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function ConfigLayout({
+export default async function ConfigLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+  if (!session) redirect("/");
   return (
     <>
       <Link href="/config">
         <h1 className="text-sm font-medium hover:underline">Config</h1>
       </Link>
-      {children}
+      {session && children}
     </>
   );
 }
--- src/app/forms/layout.tsx
+++ src/app/forms/layout.tsx
@@ -1,16 +1,20 @@
+import { redirect } from "next/navigation";
 import Link from "next/link";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function FormLayout({
+export default async function FormLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+  if (!session) redirect("/");
   return (
     <>
       <Link href="/config">
         <h1 className="text-sm font-medium hover:underline">Form</h1>
       </Link>
-      {children}
+      {session && children}
     </>
   );
 }
