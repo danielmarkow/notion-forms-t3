3ed2c8d98bad931832d95986a2629d38c712d59a
Daniel Markow <d_markow@icloud.com>
2023-12-31 11:22:27 +0100

    removed unused import and boilerplate comment

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,5 @@
 import { relations, sql } from "drizzle-orm";
 import {
-  bigint,
   boolean,
   index,
   int,
@@ -12,12 +11,6 @@ import {
 } from "drizzle-orm/mysql-core";
 import { type AdapterAccount } from "next-auth/adapters";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const mysqlTable = mysqlTableCreator((name) => `notionf_${name}`);
 
 export const notionApiKeys = mysqlTable(
